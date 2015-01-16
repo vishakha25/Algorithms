53d9fd57f2082dfb95fa06b6a13668eaa60fad3f
VISHAKHA GUPTA <vishakha.gupta.ms@gmail.com>
2015-01-16 17:02:01 -0500

    handling negative numbers

--- Algorithm/src/addDigits.java
+++ Algorithm/src/addDigits.java
@@ -1,26 +1,34 @@
 
 public class addDigits {
-	
 	public int addNum(int num){
+		/*adding digits of a number,
+		 * For example: 123 -> Result:6 <- 1+2+3
+		 */
 		if(num<0){
+			//making number positive
 			num=0-num;
 		}
 		int result=0;
-		int rem;
+		//int rem;
 		while(num>0){
-			rem=num%10;
-			result+=rem;
+		//	rem=num%10;
+			//result+=rem;
+			result+=(num%10);
 			num=num/10;
 		}
 		if(result>9){
+			/*if the result has more than 1 digit,
+			* reursive call the function until single digit left in sum
+			*/
 			System.out.println(result);
 			result=addNum(result);
 		}
 		return result;	
 	}
 	
-	
 	public int addNumRec(int num){
+		//recursive way of doing same algorithm above
+		//can also add check for negative numbers
 		int result=addN(num);
 		if (result>9){
 			result=addNumRec(result);
@@ -29,19 +37,16 @@ public class addDigits {
 	}
 	
 	int addN(int n){
-		int result;
 		if(n<1)
 			return 0;
 		else
 			return n%10+addN(n/10);
 	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		addDigits a1=new addDigits();
 		//System.out.println(a1.addNum(1999945));
 		System.out.println(a1.addNumRec(99));
-		
 	}
 
 }
